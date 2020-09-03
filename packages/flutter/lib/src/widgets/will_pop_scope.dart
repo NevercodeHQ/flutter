95a9cc62266ff9b3b4f114178da0e0076181ca41
Taha Tesser <tessertaha@gmail.com>
2020-09-03 12:39:44 +0530

    willpopscope sample & update doc

--- packages/flutter/lib/src/widgets/will_pop_scope.dart
+++ packages/flutter/lib/src/widgets/will_pop_scope.dart
@@ -11,6 +11,81 @@ import 'routes.dart';
 /// Registers a callback to veto attempts by the user to dismiss the enclosing
 /// [ModalRoute].
 ///
+/// WillPopScope prevents gestures that makes current pops such as swipe to go back
+/// gesture on iOS. To enable this behaviour use custom route which overrides
+/// `hasScopedWillPopCallback` getter.
+///
+/// {@tool dartpad --template=freeform}
+///
+/// This sample demonstrates how to implement WillPopScope and prevent user from popping
+/// current route.
+///
+/// ```dart
+/// /// Flutter code sample for WillPopScope
+/// /// This sample demonstrates how to register a callback for when the user wants
+/// /// to dismiss the current Route.
+///
+/// /// `onWillPop` callback can be used to show warning to the user on popping
+/// /// the current [Route], preventing user from going to back to unintended Routes.
+///
+/// import 'package:flutter/material.dart';
+///
+/// void main() => runApp(MyApp());
+///
+/// class MyApp extends StatelessWidget {
+///   static const String _title = 'WillPopScope Code Sample';
+///   @override
+///   Widget build(BuildContext context) {
+///     return MaterialApp(
+///       title: _title,
+///       home: IntialPage(),
+///     );
+///   }
+/// }
+///
+/// class IntialPage extends StatelessWidget {
+///   IntialPage({Key key}) : super(key: key);
+///   @override
+///   Widget build(BuildContext context) {
+///     return new Scaffold(
+///       appBar: AppBar(
+///         title: const Text('WillPopScope Demo'),
+///       ),
+///       body: Container(
+///         alignment: Alignment.center,
+///         child: RaisedButton(
+///           onPressed: () {
+///             Navigator.push(
+///                 context, MaterialPageRoute(builder: (context) => HomePage()));
+///           },
+///           child: const Text('Go to HomePage', style: TextStyle(fontSize: 20)),
+///         ),
+///       ),
+///     );
+///   }
+/// }
+///
+/// class HomePage extends StatelessWidget {
+///   HomePage({Key key}) : super(key: key);
+///   @override
+///   Widget build(BuildContext context) {
+///     return WillPopScope(
+///       onWillPop: () async {
+///         // Add your code here.
+///         return Future.value(false);
+///       },
+///       child: new Scaffold(
+///         appBar: AppBar(
+///           title: const Text('HomePage'),
+///         ),
+///         body: Center(
+///             child: Text('WillPopScope will now prevent you from going back')),
+///       ),
+///     );
+///   }
+/// }
+/// ```
+///
 /// See also:
 ///
 ///  * [ModalRoute.addScopedWillPopCallback] and [ModalRoute.removeScopedWillPopCallback],
@@ -24,8 +99,8 @@ class WillPopScope extends StatefulWidget {
     Key key,
     @required this.child,
     @required this.onWillPop,
-  }) : assert(child != null),
-       super(key: key);
+  })  : assert(child != null),
+        super(key: key);
 
   /// The widget below this widget in the tree.
   ///
